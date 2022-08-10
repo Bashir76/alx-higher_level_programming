c35e7d76aaa5ab84fe12637f36bf988dac812d31
bashir76 <ameekn76@gmail.com>
2022-08-10 03:47:50 -0700

    my files

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+--prints all rows in first_table of hbtn_0c_0
+SELECT * FROM `first_table`;
---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+--Create second_table in the database hbtn_0c_0 with id, name and score
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO (`id`, `name`, `score`) VALEUS(1, "John", 10)
+INSERT INTO (`id`, `name`, `score`) VALEUS(2, "Alex", 3)
+INSERT INTO (`id`, `name`, `score`) VALEUS(3, "Bob", 14)
+INSERT INTO (`id`, `name`, `score`) VALEUS(4, "George", 8)
