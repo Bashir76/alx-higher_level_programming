98136686e203a914cd8926e387ab1fed58a75491
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-08-11 10:00:12 +0100

    Update 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
---Create second_table in the database hbtn_0c_0 with id, name and score
+-- Creates and fills a table second_table with attributes id, name and score.
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO (`id`, `name`, `score`) VALUES (1, "John", 10)
 INSERT INTO (`id`, `name`, `score`) VALUES (2, "Alex", 3)
