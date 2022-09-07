8010b0c7ec9b035d2dd53894eadd28c19ef0c054
bashir76 <ameerkn76@gmail.com>
2022-09-07 09:40:45 +0000

    third commit

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,12 +1,3 @@
 #!/usr/bin/node
-<<<<<<< HEAD
-<<<<<<< HEAD
-const myVar = 'Javascript is amazing';
-=======
-const myVar = "JavaScript is amazing”;
->>>>>>> 3b8ec52773b29cceae30a6d7b4eeb878ac41efd5
+const myVar = “JavaScript is amazing”;
 console.log(myVar);
-=======
-const myVar = "JavaScript is amazing”;
-console.log(myVar)
->>>>>>> 00eb5c53ac8f9ea3a2386eec46bd26729f869995
