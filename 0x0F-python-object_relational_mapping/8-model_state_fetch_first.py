1a02f1a1af75253aadb1fb34861f0d90b23ae1c8
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-09-10 16:51:35 +0100

    Update 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
 Script that lists all State objects that contain the 
-
-letter a from the database hbtn_0e_6_usa
+letter a
 """
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
