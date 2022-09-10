e94f03dc5cafd61b078d770c7872d91e8b4ab759
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-09-10 16:57:31 +0100

    Update 9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa.
+Script that lists all State objects that contain the letter a
+@author: Bashir Amirkano
 """
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
@@ -10,23 +11,22 @@ import sys
 
 if __name__ == '__main__':
     args = sys.argv
-    if len(args) != 5:
+    if len(args) != 4:
         print("Usage: {} username password database_name".format(args[0]))
         exit(1)
     username = args[1]
     password = args[2]
     data = args[3]
-    state_name = args[4]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-     .format(username, password, data))
+                           .format(username, password, data))
     # create custom session object class from database engine
     Session = sessionmaker(bind=engine)
     # create instance of new custom session class
     session = Session()
-    states = session.query(State).filter(State.name == state_name)\
+    states = session.query(State).filter(State.name.contains('a'))\
                     .order_by(State.id)
-    if states is not None and states.count() > 0:
+    if states is not None:
         for state in states:
-            print('{}'.format(state.id))
+            print('{}: {}'.format(state.id, state.name))
     else:
-        print('Not found')
+        print('Nothing')
