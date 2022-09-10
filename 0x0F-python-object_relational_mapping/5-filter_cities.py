74e231b2e07ddb703365f1316f73c9a716717ca2
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-09-10 15:46:35 +0100

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,7 @@
 #!/bin/usr/python3
 """
-python file that contains the class definition of a State and an instance Base = declarative_base()
+   Python file that contains the class definition of a State and an instance 
+   Base = declarative_base().
 """
 import MySQLdb
 import sys
