db8e6ca521e0e7f18520059f0eee2714a7234cbc
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-09-10 16:35:34 +0100

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,25 +3,32 @@
 Script that lists all State objects 
 from the database hbtn_0e_6_usa.
 """
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
+import MySQLdb
 import sys
 
 
 if __name__ == '__main__':
     args = sys.argv
-    if len(args) != 4:
-      print("Usage: {} username password database_name".format(args[0]))
-       exit(1)
+    if len(args) != 5:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
     username = args[1]
     password = args[2]
     data = args[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                                .format(username, password, data))
-    # create custom session object class from database engine
-    Session = sessionmaker(bind=engine)
-    # create instance of new custom session class
-    session = Session()
-    for state in session.query(State).order_by(State.id):
-       print('{}: {}'.format(state.id, state.name))
+    state_name = args[4]
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=data, port=3306)
+    cur = db.cursor()
+    num_rows = cur.execute("SELECT cities.name FROM cities WHERE state_id =\
+                           (SELECT id FROM states WHERE name LIKE BINARY %s)\
+                           ORDER BY cities.id;", (state_name, ))
+    rows = cur.fetchall()
+    i = 1
+    for row in rows:
+        print(row[0], end='')
+        if i < num_rows:
+            print(end=', ')
+        i += 1
+    print()
+    cur.close()
+    db.close()
