2b92c88d8ceb6388d0464354bb0f3c2b89f38fd9
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-09-10 17:18:22 +0100

    Update 13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Script that deletes all State objects with a name containing the 
-letter a from the database
+Script that deletes all State objects with a name containing the letter a
+
 @:
 """
 from model_state import Base, State
