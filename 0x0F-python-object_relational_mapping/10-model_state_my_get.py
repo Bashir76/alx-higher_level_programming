727c93344957bed37b5b6d355c8ace9542330c52
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-09-10 17:01:00 +0100

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that adds the State object “Louisiana” to the database hbtn_0e_6_usa.
+Script that prints the State object with the name passed as argument
+@
 """
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
@@ -10,20 +11,23 @@ import sys
 
 if __name__ == '__main__':
     args = sys.argv
-    if len(args) != 4:
+    if len(args) != 5:
         print("Usage: {} username password database_name".format(args[0]))
         exit(1)
     username = args[1]
     password = args[2]
     data = args[3]
+    state_name = args[4]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-       .format(username, password, data))
+                           .format(username, password, data))
     # create custom session object class from database engine
     Session = sessionmaker(bind=engine)
     # create instance of new custom session class
     session = Session()
-    new_state = State()
-    new_state.name = 'Louisiana'
-    session.add(new_state)
-    session.commit()
-    print(new_state.id)
+    states = session.query(State).filter(State.name == state_name)\
+                    .order_by(State.id)
+    if states is not None and states.count() > 0:
+        for state in states:
+            print('{}'.format(state.id))
+    else:
+        print('Not found')
