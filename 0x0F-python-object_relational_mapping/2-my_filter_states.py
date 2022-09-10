5ee940bce3b44db95efddd084db182de295952dd
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-09-10 15:34:07 +0100

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,20 +3,26 @@
 """
     Display state data from database_name.
 """
- if __name__ == '__main__':
-  args = sys.argv
-  if len(args) != 5:
-    print("usage: {} username password database_name".format(args[0]))
-    exit(1)
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 5:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
     username = args[1]
     password = args[2]
     data = args[3]
     state_name = args[4]
-    db = MYSQLdb.connect(host='localhost', user=username, passwd=password, db=data, port=3306)
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=data, port=3306)
     cur = db.cursor()
-    num_row = cur.execute('SELECT * FROM states WHERE states.name LIKE BINARY/'{}' ORDER BY states.id;".format(state_name))
-    row = cur.fetchall()
-    for row in rows;
-    print(row)
+    num_rows = cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY\
+                           '{}' ORDER BY states.id;".format(state_name))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
     cur.close()
     db.close()
