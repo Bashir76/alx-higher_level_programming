7ec8f418c1e959bee4910ba1b6f370d2fcfcd07e
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-09-10 17:22:31 +0100

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-A python file that contains the class definition of a State and an instance 
-
-@:
+A file that contains the class definition of a State and an instance
+@author: Bashir Amirkano
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
