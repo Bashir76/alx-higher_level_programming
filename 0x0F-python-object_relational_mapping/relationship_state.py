19277e801768db39ec91e3fba1b34bb0fa93a109
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-09-12 09:35:52 +0100

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 A new table into de data base for state representation
-@AmirKano5:
+@author: Amir Kano5 
 """
 import sqlalchemy
 from sqlalchemy import Column, Integer, String
