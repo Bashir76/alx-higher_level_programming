e8069b0a468ef6ea3ebdf3a3de37665d468b5d00
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-09-26 09:43:19 +0100

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
+"""A script that:
+- takes in a URL,
+- sends a request to the URL
+- displays the body of the response (decoded in utf-8).
 """
-- Python script that takes in a URL
-- author: Bashir AmirKano 
-"""
-from urllib.request import urlopen
-from urllib.error import HTTPError
-from sys import argv
 
 
 if __name__ == "__main__":
-    url = argv[1]
+    import sys
+    from urllib import request, error
+
     try:
-        with urlopen(url) as response:
-            print(response.read().decode('utf-8'))
-    except HTTPError as e:
-        print('Error code: {}'.format(e.code))
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
