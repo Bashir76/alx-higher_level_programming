b149643be1f1855be7edc53c3e831c7e80d7fe77
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-12-26 17:57:21 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,18 +1,24 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-  }
-});
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
+const movieId = process.argv[2];
+const myList = [];
+let completedRequests = 0;
+
+function getCharNames (charIdList) {
+  for (let i = 0; i < charIdList.length; i++) {
+    request.get(charIdList[i], (e, r, body) => {
+      myList[i] = JSON.parse(body).name;
+      completedRequests++;
+      if (completedRequests === charIdList.length) {
+        for (let i = 0; i < myList.length; i++) {
+          console.log(myList[i]);
+        }
       }
-    }
+    });
+  }
+}
+
+request
+  .get('http://swapi.co/api/films/' + movieId, 'utf8', (e, r, body) => {
+    getCharNames(JSON.parse(body).characters);
   });
